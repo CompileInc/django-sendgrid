8fcba488fafd8ba62fbceb180be200c911c0e7bf
Dilu Sasidharan <dilu@compile.com>
2014-09-03 15:26:38 +0530

    Fix: sendgrid_email_backend changed to mailer_email_backend for celery support

--- sendgrid/mail.py
+++ sendgrid/mail.py
@@ -8,7 +8,7 @@ from django.core.mail import send_mail
 from utils import in_test_environment
 
 
-SENDGRID_EMAIL_BACKEND = getattr(settings, "SENDGRID_EMAIL_BACKEND", None)
+SENDGRID_EMAIL_BACKEND = getattr(settings, "MAILER_EMAIL_BACKEND", None)
 
 logger = logging.getLogger(__name__)
 
@@ -20,10 +20,10 @@ def get_sendgrid_connection(*args, **kwargs):
 	if in_test_environment():
 		logger.debug("In test environment!")
 		backend = "django.core.mail.backends.locmem.EmailBackend"
-		
+
 	logger.debug("Getting SendGrid connection using backend {b}".format(b=backend))
 	sendgrid_connection = get_connection(backend, *args, **kwargs)
-	
+
 	return sendgrid_connection
 
 def send_sendgrid_mail(subject, message, from_email, recipient_list,
@@ -40,6 +40,6 @@ def send_mass_sendgrid_mail(datatuple, fail_silently=False, auth_user=None, auth
 	Sends mass mail with SendGrid.
 	"""
 	raise NotImplementedError
-	
+
 	sendgrid_connection = get_sendgrid_connection()
 	return send_mass_mail(datatuple, fail_silently, auth_password, auth_password, connection=sendgrid_connection)
